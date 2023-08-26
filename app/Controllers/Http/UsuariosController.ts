2ca7d558e9e723bf48626db48a8250adfb305dab
Frank Cairampoma <ingfralch@gmail.com>
2023-08-26 11:42:10 -0500

    subiendo usuario a base de datos

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,10 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Usuario from 'App/Models/Usuario'
 
 export default class UsuariosController {
-  public async getUsuarios({}: HttpContextContract) {}
+  public async getUsuarios({response}: HttpContextContract) {
+    const usuarios = await Usuario.all()
+    return response.status(200).json(usuarios)
+  }
 
   public async setUsuarios({request, response}: HttpContextContract) {
-    const { nombre, apellido, email, password } = request.all()
-    return response.json({ nombre, apellido, email, password })
+    const { dni, nombre, apellido, celular, cargo, area } = request.all()
+    Usuario.create({ dni, nombre, apellido, celular, cargo, area })
+    return response.status(200).json({ message: 'Usuario creado' })
   }
 }
