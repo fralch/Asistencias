e50fcd8101a7ff0baa54c72c6b55d567254cdfb7
Fralch <ingfralch@gmail.com>
2023-10-11 17:57:29 -0500

    quitando lo que no se usa

--- app/Controllers/Http/RegistarTardanzasFaltasController.ts
+++ app/Controllers/Http/RegistarTardanzasFaltasController.ts
@@ -2,8 +2,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import RegistroAsistencia from 'App/Models/RegistroAsistencia'
 import Usuario from 'App/Models/Usuario'
 import Falta from 'App/Models/Falta'
-import Tardanza from 'App/Models/Tardanza'
-import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class RegistarTardanzasFaltasController {
 
