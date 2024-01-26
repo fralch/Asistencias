4e918bba27b0adb1ed5fe96f82e5286282d3f2c4
Frank Cairampoma <ingfralch@gmail.com>
2024-01-26 17:48:17 -0500

    tardanzas done

--- app/Controllers/Http/TardanzasController.ts
+++ app/Controllers/Http/TardanzasController.ts
@@ -6,18 +6,17 @@ import db from '@ioc:Adonis/Lucid/Database'
 
 export default class TardanzasController {
 
-  public async getTardanzas({response}: HttpContextContract) {
-    const tardanzas = await Tardanza.all()
-    return response.status(200).json(tardanzas)
-  }
-
   public async getTardanza({ params, response }: HttpContextContract) {
     const usuario_id = await Usuario.findBy('dni', params.dni)
     // obtener todas tardanzas de usuario
     console.log(usuario_id?.id)
-    const totalmin = await db.query().from('tardanzas').where('usuario_id', 1).sum('minutos as minutos').orderBy('usuario_id', 'desc')
-
-    return response.status(200).json(totalmin)
+    
+    if (usuario_id?.id) {
+      const totalmin = await db.query().from('tardanzas').where('usuario_id', usuario_id.id).sum('minutos as minutos').orderBy('usuario_id', 'desc')
+      return response.status(200).json(totalmin)
+    } else {
+      return response.status(404).json({ message: 'Usuario no encontrado' })
+    }
   }
   
 }
